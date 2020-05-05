8154eac28e80f7c6a43218b1ba2158a8c9d19676
Camila Oliveira <camila.oliveira@agrotis.com>
2020-05-04 19:46:27 -0700

    fixes to deploy

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
-import VueSimpleAlert from "vue-simple-alert";
 
 Vue.config.productionTip = false
-Vue.use(VueSimpleAlert);
 
 new Vue({
   router,
